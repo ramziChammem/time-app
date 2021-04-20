822a6e387144cdde9ff2d69e411eaabf97b91c21
ramziChammem <ramzichammem69@gmail.com>
2021-04-20 04:24:49 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Helelews"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("click"),
+      ),
+
+    ),
+
+  ));
+}
+
